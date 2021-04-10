1d57365c328937b7e357974069fec5f4c5ff7a9f
tony-ojeda <tojeda96@gmail.com>
2021-04-09 20:38:56 -0500

    test validate store and update

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -2,14 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Repository;
 use Illuminate\Http\Request;
 
 class RepositoryController extends Controller
 {
 	public function store(Request $request)
 	{
+		$request->validate([
+			'url' => 'required',
+			'description' => 'required'
+		]);
+
 		$request->user()->repositories()->create($request->all());
 
 		return redirect()->route('repositories.index');
+	}
+
+	public function update(Request $request, Repository $repository)
+	{
+		$request->validate([
+			'url' => 'required',
+			'description' => 'required'
+		]);
+
+		$repository->update($request->all());
+
+		return redirect()->route('repositories.edit', $repository);
 	}
 }
--- tests/Feature/Http/Controller/RepositoryControllerTest.php
+++ tests/Feature/Http/Controller/RepositoryControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Http\Controller;
 
+use App\Models\Repository;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -37,5 +38,46 @@ class RepositoryControllerTest extends TestCase
 			->assertRedirect('repositories');
 
 		$this->assertDatabaseHas('repositories', $data);
+	}
+
+	public function test_update()
+	{
+		$repository = Repository::factory()->create();
+
+		$data = [
+			'url' => $this->faker->url,
+			'description' => $this->faker->text,
+		];
+
+		$user = User::factory()->create();
+
+		$this->actingAs($user)
+			 ->put("repositories/$repository->id", $data)
+			 ->assertRedirect("repositories/$repository->id/edit");
+
+		$this->assertDatabaseHas('repositories', $data);
+	}
+
+	public function test_validate_store()
+	{
+		$user = User::factory()->create();
+
+		$this
+			->actingAs($user)
+			->post('repositories', [])
+			->assertStatus(302)
+			->assertSessionHasErrors(['url', 'description']);
+	}
+
+	public function test_validate_update()
+	{
+		$repository = Repository::factory()->create();
+
+		$user = User::factory()->create();
+
+		$this->actingAs($user)
+			 ->put("repositories/$repository->id", [])
+			->assertStatus(302)
+			->assertSessionHasErrors(['url', 'description']);
 	}
 }
